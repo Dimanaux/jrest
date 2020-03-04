0a7c07f945e7ad3adcb8b6a718df1d8d2a58aff4
Dmitry Barskov <barskoidim@gmail.com>
2020-03-04 21:20:23 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/jrest/JrestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JrestApplication
+
+fun main(args: Array<String>) {
+    runApplication<JrestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jrest/JrestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JrestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
